178ef9b3cff837815adc72d7fde2674a2e8e0b2e
Erik Lindqvist <eriklindqvist@users.noreply.github.com>
2018-02-07 10:40:43 +0100

    Tag image to use local docker repo

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o recipe .
-docker build -t recepies .
+docker build -t recepies_api .
+docker tag recepies_api:latest proto:5000/recepies_api
